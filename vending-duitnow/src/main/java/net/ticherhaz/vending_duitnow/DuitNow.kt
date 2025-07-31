81af358eff9f0df02141c1f61668df998f805234
Ticherhaz <ticherhaz@gmail.com>
2025-07-31 13:51:50 +0800

    147

--- vending-duitnow/src/main/java/net/ticherhaz/vending_duitnow/DuitNow.kt
+++ vending-duitnow/src/main/java/net/ticherhaz/vending_duitnow/DuitNow.kt
@@ -127,8 +127,8 @@ class DuitNow(
 
                     // Set dialog dimensions (90% width, 50% height)
                     val displayMetrics = context.resources.displayMetrics
-                    val width = (displayMetrics.widthPixels * 0.90).toInt()
-                    val height = (displayMetrics.heightPixels * 0.80).toInt()
+                    val width = (displayMetrics.widthPixels * 0.70).toInt()
+                    val height = (displayMetrics.heightPixels * 0.70).toInt()
 
                     setLayout(width, height)
 
