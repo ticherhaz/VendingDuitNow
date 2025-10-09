cda5fc0ee9a6643f8fe8f48f7979b01770838c5e
Ticherhaz <ticherhaz@gmail.com>
2025-10-09 14:32:07 +0800

    153, adjust UI Main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "net.ticherhaz.vendingduitnow"
         minSdk = 21
         targetSdk = 36
-        versionCode = 152
-        versionName = "1.5.2"
+        versionCode = 153
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
